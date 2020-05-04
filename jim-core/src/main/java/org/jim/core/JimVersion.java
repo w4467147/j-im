ebf707b27b2d2092aa38cff489b6a354bd8282f1
王超 <wangchao@wangchao.mac.pro>
2020-05-04 21:11:52 +0800

    发布3.0.0.v20200501-RELEASE

--- jim-core/src/main/java/org/jim/core/JimVersion.java
+++ jim-core/src/main/java/org/jim/core/JimVersion.java
@@ -6,5 +6,5 @@ package org.jim.core;
  * @date 2020-05-02 02:03
  */
 public class JimVersion {
-    public static final String version = "3.0.0.v20200101-RELEASE";
+    public static final String version = "3.0.0.v20200501-RELEASE";
 }
